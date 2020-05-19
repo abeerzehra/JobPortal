4e0b25657f7e13c179a01978235b49c44fe78a3e
abeerzehra <k164068@nu.edu.pk>
2020-05-20 02:33:30 +0500

    added credentials

--- company/accept-user.php
+++ company/accept-user.php
@@ -54,8 +54,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'seanbrocks2@gmail.com';                     // SMTP username
-    $mail->Password   = 'University123';                               // SMTP password
+    $mail->Username   = 'abeer.zehra92@gmail.com';                     // SMTP username
+    $mail->Password   = 'Dangling92';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; 
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for 
 
--- company/reject-user.php
+++ company/reject-user.php
@@ -31,13 +31,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'seanbrocks2@gmail.com';                     // SMTP username
-    $mail->Password   = 'University123';                               // SMTP password
+    $mail->Username   = 'abeer.zehra92@gmail.com';                     // SMTP username
+    $mail->Password   = 'Dangling92';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; 
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for 
 
     //Recipients
-    $mail->setFrom('khdhackerboy@gmail.com', 'Fastians JobPortal');
+    $mail->setFrom('abeer.zezhra92@gmail.com', 'Fastians JobPortal');
     $mail->addAddress($E);     // Add a recipient
     
 
